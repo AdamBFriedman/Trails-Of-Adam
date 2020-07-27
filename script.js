cae8073f3c2472d7b1c07a12ed0f3c19fd13d864
Adam Friedman <adam@blok.party>
2020-07-27 14:31:14 -0700

    Added particles class

--- script.js
+++ script.js
@@ -32,3 +32,42 @@ window.addEventListener('mousemove', function (e) {
 	mouse.x = event.x;
 	mouse.y = event.y;
 });
+
+// Particles
+class Particle {
+	constructor(x, y, radius) {
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.color = gradient1;
+		this.text = lettersArray[Math.floor(Math.random() * lettersArray.length)];
+	}
+	draw() {
+		ctx.strokeStyle = 'rgba(0,0,0,0.2)';
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+		ctx.fillStyle = this.color;
+		ctx.closePath();
+		ctx.fill();
+		ctx.stroke();
+
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, this.radius * 0.8, 0, Math.PI * 1.5, true);
+		ctx.fillStyle = gradient2;
+		ctx.fill();
+
+		ctx.fillStyle = gradient2;
+		ctx.font = this.radius + 'px Verdana';
+		ctx.fillText(this.text, this.x - this.radius / 2.7, this.y);
+	}
+	update() {
+		// Autopilot when mouse leaves canvas
+		if (mouse.x == undefined && mouse.y == undefined) {
+			let newX = radius * 2 * Math.cos(mouse.autopilotAngle * (Math.PI / 180));
+			let newY = radius * 0.9 * Math.sin(mouse.autopilotAngle * (Math.PI / 90));
+			mouse.x = newX + canvas.width / 2;
+			mouse.y = newY + canvas.height / 2;
+		}
+		mouse.autopilotAngle += 0.01;
+	}
+}
