67f9e621a35d77a88a27492aee71f4ca3391ed6c
Adam Friedman <adam@blok.party>
2020-07-27 14:17:51 -0700

    Added gradient variables

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ canvas.height = window.innerHeight;
 ctx.textBaseline = 'middle';
 let lettersArray = ['A', 'D', 'A', 'M'];
 let hue = 0;
+
+// Gradients
+const gradient1 = ctx.createLinearGradient(0, 0, canvas.width, 0);
+gradient1.addColorStop(0.49, 'black');
+gradient1.addColorStop(0.51, 'white');
+
+const gradient2 = ctx.createLinearGradient(0, 0, canvas.width, 0);
+gradient2.addColorStop(0.49, 'white');
+gradient2.addColorStop(0.51, 'rgba(0,0,0,0.7)');
