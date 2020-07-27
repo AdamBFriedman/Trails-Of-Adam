37911e12a9648885171156bf0afc61a30526d63a
Adam Friedman <adam@blok.party>
2020-07-27 14:14:03 -0700

    Added variables and favicon

---
+++ script.js
@@ -0,0 +1,8 @@
+// Variables
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.textBaseline = 'middle';
+let lettersArray = ['A', 'D', 'A', 'M'];
+let hue = 0;
