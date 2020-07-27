6bcece1c6aa90cefc33f69783a9564b5d152ddd4
Adam Friedman <adam@blok.party>
2020-07-27 14:21:04 -0700

    Added mouse tracking

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ gradient1.addColorStop(0.51, 'white');
 const gradient2 = ctx.createLinearGradient(0, 0, canvas.width, 0);
 gradient2.addColorStop(0.49, 'white');
 gradient2.addColorStop(0.51, 'rgba(0,0,0,0.7)');
+
+let particles = [];
+let numberOfParticles = 100;
+let radius = window.innerWidth / 5;
+
+// Track mouse
+const mouse = {
+	x: 0,
+	y: 0,
+	radius: 60,
+	autopilotAngle: 0,
+};
+
+window.addEventListener('mousemove', function (e) {
+	mouse.x = event.x;
+	mouse.y = event.y;
+});
